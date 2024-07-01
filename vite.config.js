cd28e0da284da6b3e94a18c64c3b558edc38ad96
kolujAs <fernandodeibe@gmail.com>
2024-07-01 12:05:43 -0300

    Ajustos para deploy con Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/interval_todolist/'  // Asegúrate de que este sea el nombre correcto del repositorio
 })
